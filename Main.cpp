762aebe3e312a9186210a7728375d0c26d3592ac
thang <vietthang11012002@gmail.com>
2023-11-15 10:59:41 +0700

    push bai thi

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include "Header.h"
+
+int main()
+{
+	int a[MAX];
+	nhap(a, 10);
+	//Goi ham xuat mang dao nguoc
+	xuatMangDaoNguoc(a);
+	return 0;
+}
---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+void nhap(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+void xuatSV(){
+	cout<<"DH52006168|HoVietThang|D20_TH10";
+}
